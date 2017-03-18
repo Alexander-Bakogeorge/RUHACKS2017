4d3cf2306271b8a1d5f9f5e8e844dd64f1e9814c
Tudor Tibu <tudortibu@gmail.com>
2017-03-18 16:16:23 -0400

    Habitat

---
+++ RUHacksUnity/Assets/HorseAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseAnimation : MonoBehaviour {
+
+    Animator anim;
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        anim = this.GetComponent<Animator>();
+        rb = this.GetComponent<Rigidbody>();
+        anim.SetFloat("Speed", rb.velocity.magnitude);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetFloat("Speed", rb.velocity.magnitude);
+        print(rb.velocity.magnitude);
+
+    }
+}
